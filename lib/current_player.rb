1c40da8b3b8c591c05dfcc7bcb4eee76e7a9a136
Dominik Szarkiel <d.szarkiel@gmail.com>
2020-09-21 18:30:18 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,11 @@
+def turn_count(board)
+  counter = 0
+  board.each do |space|
+    if space != " "
+      counter += 1
+    end
+  end
+  return counter
+end
+
+def current_player(board) = turn_count(board).even? ? return "X" : "O"
