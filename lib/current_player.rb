1fac7eec394763ba1be8080f6cd55c971d35a625
Dominik Szarkiel <d.szarkiel@gmail.com>
2020-09-21 18:33:05 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,4 +8,10 @@ def turn_count(board)
   return counter
 end
 
-def current_player(board) = turn_count(board).even? ? return "X" : "O"
+def current_player(board)
+  if turn_count(board).even?
+    return "X"
+  else
+    return "O"
+  end
+end
